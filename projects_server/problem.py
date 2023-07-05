95cd1cb96f9a47e9e195faf1e5a46b8d1e9dc08b
Jong Hyun Park <jhpark@infomagix.com>
2023-07-05 21:04:23 +0900

    project server.

---
+++ projects_server/problem.py
@@ -0,0 +1,204 @@
+from flask import send_file
+import json 
+
+class Problem:
+    question = ""
+    inputValue = None
+    answerValue = []
+    isCorrect = None 
+
+    # construct all problems
+    @staticmethod
+    def factoryFromJson(filepath):
+        with open(filepath, 'r', encoding='utf-8') as f:
+            jsonData = json.load(f)
+            problems = []
+            for problem in jsonData['problems']:
+                problems.append( Problem.fromJson(problem))
+            return problems
+
+        assert(False)
+
+    # construct "a" problem
+    @staticmethod
+    def fromJson(jsonObject):
+        if jsonObject['answer']['type'] == "value":
+            return ValueProblem(jsonObject['question'], jsonObject['answer'])
+        elif jsonObject['answer']['type'] == "table":
+            return TableProblem(jsonObject['question'], jsonObject['answer'])
+        elif jsonObject['answer']['type'] == "graph":
+            return GraphProblem(jsonObject['question'], jsonObject['answer'])
+
+
+
+        assert(False)
+
+    def genView(self):
+        assert(False) # interface.
+
+    def setValue(self, formDict):
+        assert(False)
+
+    def testAnswer(self, input, answer):
+        ret = (input == answer)
+        if not ret:
+            try:
+                ret = float(input).round(1) == float(answer).round(1)
+            except:
+                ret = False
+                print("Parsing ERROR!")
+        return ret
+
+    def validTag(self, isValid):
+        if isValid is None:
+            return ""
+        elif isValid:
+            return "is-valid"
+        else:
+            return "is-invalid"
+    
+
+class ValueProblem(Problem):
+    def __init__(self, question, answer):
+        self.question = question
+        self.answerValue = answer['value']
+        self.inputValue = ""
+    
+    def genView(self):
+        return f"""
+        <div class="container">
+        <h3>Q. {self.question}</h3>
+        <div class="form-group" style="width: 500px;">
+            <input type="text" name="{self.question}" value="{self.inputValue}" class="form-control {self.validTag(self.isCorrect)}" aria-describedby="helpInline">
+        </div>
+        </div>
+        """
+    def setValue(self, formDict):
+        self.inputValue = formDict[self.question]
+        self.isCorrect = self.testAnswer(self.inputValue, self.answerValue)
+    
+    def isAllCorrect(self):
+        return self.isCorrect
+
+
+class TableProblem(Problem):
+
+    def __init__(self, question, answer):
+        self.question = question
+        self.answerValue = (answer['value'])
+        self.inputValue = {}
+
+    def genView(self):
+        tableHeader = self.genTableHeader()
+        tableBody = self.genTableBody() 
+
+        return f"""
+        <div class="container">
+        <h3>Q. {self.question}</h3>
+        <table class="table">
+        {tableHeader}
+        {tableBody}
+        </table>
+    </div>
+        """
+
+
+    def genTableHeader(self):
+        ret = ""
+        for key in self.answerValue.keys():
+            if key == 'index':
+                ret += f"<th>---</th>"
+            else:
+                ret += f"<th>{key}</th>"
+
+        return f"""<thead>
+                <tr>{ret}</tr>
+            </thead>"""
+
+    def genTableBody(self):
+        ret = "<tbody>"
+        for value in self.answerValue['index']:
+            ret += "<tr>"
+            for key in self.answerValue.keys():
+                if key == 'index':
+                    ret += f'<td>{value}</td>'
+                else:
+                    name = self.question+'_'+value+'_'+key
+                    inputValue = ""
+                    if name in self.inputValue:
+                        inputValue = self.inputValue[name]
+
+                    if self.isCorrect is None:
+                        correct = None
+                    elif  name in self.isCorrect:
+                        correct = self.isCorrect[name]
+                    else:
+                        correct = None
+                    
+                    ret += f'''<td><input type="text"
+                     name="{name}"
+                     value="{inputValue}"
+                     class="form-control {self.validTag(correct)}"></td>'''
+            ret+="</tr>"
+        ret +="</tbody>"
+        return ret
+
+    def setValue(self, formDict):
+        if self.isCorrect is None:
+            self.isCorrect = {}
+
+        for value in self.answerValue['index']:
+            for key in self.answerValue.keys():
+                if key != 'index':
+                    name=f"{self.question}_{value}_{key}"
+                    self.inputValue[name] = formDict[name]
+                    self.isCorrect[name] = self.testAnswer(self.inputValue, self.answerValue)
+    
+    def isAllCorrect(self):
+        ret = False 
+        for value in self.answerValue['index']:
+            for key in self.answerValue.keys():
+                if key != 'index':
+                    name=f"{self.question}_{value}_{key}"
+                    if name in self.isCorrect:
+                        if not self.isCorrect[name]:
+                            return False
+                        else:
+                            ret = True
+        return ret
+
+
+class GraphProblem(Problem):
+    def __init__(self, question, answer):
+        self.question = question
+        self.answerValue = answer['value']
+    
+    def genView(self):
+        imgs = ""
+        for imgPath in self.answerValue:
+            imgRelPath = "../static/project_rsc/" + imgPath
+            imgs += '<div class="col-md-4"><img src='+ imgRelPath  + '></div>'
+
+        return f"""
+        <div class="container">
+        <h3>Q. {self.question}</h3>
+        <div class="row">
+        {imgs}
+        </div>
+        </div>
+        """
+
+    def isAllCorrect(self):
+        return True 
+
+                        
+                        
+
+
+
+# TEST!
+if __name__ == '__main__':
+    with open('data.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        problems = Problem.factoryFromJson(data)
+        print(problems)
---
+++ projects_server/run.py
@@ -0,0 +1,75 @@
+from flask import Flask, render_template, request, redirect, session
+from problem import Problem
+from datetime import datetime
+import os
+import json 
+
+problemPath = 'static/project_rsc/proj1_v2.json'
+directory = "results"
+problems = Problem.factoryFromJson(problemPath)
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key' 
+
+def saveToFile(resultDict):
+    try:
+        os.mkdir(directory)
+    except FileExistsError:
+        print("")
+
+    date_time = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    resultDict['time'] = date_time
+    file_path = os.path.join(directory, session['name']) 
+    with open(file_path, 'w', encoding='utf-8') as file:
+        json.dump(resultDict, file, ensure_ascii=False)
+
+def testLogin(name, emp_id):
+    if len(name) == 0:
+        session['logged_in'] = False 
+        return "/login" 
+    #TODO, check name to login
+    else:
+        session['logged_in'] = True 
+        return "/test" 
+
+@app.route('/')
+def index():
+    return redirect('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Process the login form data
+        session['name'] = request.form['username']
+        session['emp_id'] = request.form['password']
+
+        return redirect(testLogin(session['name'], session['emp_id']))
+    
+    # Render the login form template for GET requests
+    return render_template('login.html')
+
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    if not ("logged_in" in session):
+        return redirect('/login')
+    if not session["logged_in"]:
+        return redirect('/login')
+
+    result = ""
+    if request.method == 'POST':
+        # form data to problem
+        for problem in problems:
+            problem.setValue(request.form)
+        
+        # save to file
+        results = {}
+        for problem in problems:
+            results[problem.question] = problem.isAllCorrect()
+        
+        saveToFile(results)
+
+    return render_template('index.html', problems=problems,
+     name=session['name'], emp_id=session['emp_id'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
